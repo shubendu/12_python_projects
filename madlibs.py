24152bbfb65dcde04a6d3c354dfa8e080535e090
shubendu <biswasshubendu4@gmail.com>
2021-03-28 20:48:22 +0530

    madlib

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# youtuber = "kylie"
+
+# print("susbcribe to "+youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adjective = input("Adjective:")
+verb1 = input("verb:")
+verb2 = input("verb:")
+famous_person = input("Famous person:")
+
+madlib = f"Computer programming is so {adjective}. It makes me excited all the time becuse I love to {verb1}.\
+Stay Hydrated and {verb2} like {famous_person}."
+
+print(madlib)
